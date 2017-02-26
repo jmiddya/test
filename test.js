d689deae314d17dcdf1d334775677dbd08c8e3cc
J Middya <jmiddya.mca@gmail.com>
2017-02-27 01:38:27 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,13 @@
-const http = require('http');
+var express = require('express')
+var app = express();
 
-const hostname = 'localhost';
-const port = 3000;
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})

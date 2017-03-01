b571bd00f76a1aa432e42a457ac3c959522571f3
J Middya <jmiddya.mca@gmail.com>
2017-03-01 09:37:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -8,6 +8,62 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+//////////JM : START//////////
+var location = 'Kolkata'
+var getWeather = function ( location) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=f953e7b081a49dc14a56671ffa303848';
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  })
+    .then(rsp => {
+      var res = rsp.json();
+      return res;
+    })
+    .then(json => {
+      if (json.error && json.error.message) {
+        throw new Error(json.error.message);
+      }
+      return json;
+    });
+}
+
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+
+app.get('/webhook', function(request, response) {
+  getWeather(location).then(weatherJson => {
+		  var weatherDetails = '';
+		  	
+		  var temp = weatherJson.main.temp - 273.15;
+		  weatherDetails = weatherDetails + 'Current temperature : ' + temp + ' C';
+			
+		  var humid = weatherJson.main.humidity;
+		  weatherDetails = weatherDetails + ', Humidity : ' + humid + '%';
+				
+	    var cloud = weatherJson.clouds.all;
+		  weatherDetails = weatherDetails + ', Cloud : ' +cloud + '%';
+			
+		  var desc = weatherJson.weather[0].description;
+		  weatherDetails = weatherDetails + ', Overall weather : ' + desc;
+			
+		  weatherDetails = weatherDetails + ' in ' + location;
+    
+		  response.send(weatherDetails);
+      })
+})
+
+//////////JM : END//////////
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })

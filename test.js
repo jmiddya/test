474e4b3fc9156f7871b5f9b86837b9f03e8ae1b4
J Middya <jmiddya.mca@gmail.com>
2017-03-06 15:35:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
 })
 
 //////////JM : START//////////
-var location = 'Kolkata'
+//var location = 'Kolkata'
 var getWeather = function ( location) {
   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=f953e7b081a49dc14a56671ffa303848';
   return fetch(url, {
@@ -41,7 +41,8 @@ try {
 }
 
 app.get('/webhook', function(request, response) {
-  getWeather(location).then(weatherJson => {
+  console.log(request);	
+  getWeather(request).then(weatherJson => {
 		  var weatherDetails = '';
 		  	
 		  var temp = weatherJson.main.temp - 273.15;

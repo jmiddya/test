6739b3dd15e4259ae8ac9a9fc0e67104ba63d1e2
J Middya <jmiddya.mca@gmail.com>
2017-03-07 11:48:36 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 

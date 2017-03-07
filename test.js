557e59b81475eccbc55da2fe1a3299b5981ea18a
J Middya <jmiddya.mca@gmail.com>
2017-03-07 11:25:55 +0530

    Update test.js

--- test.js
+++ test.js
@@ -42,7 +42,7 @@ try {
 
 app.get('/webhook', function(request, response) {
   //var location = request.query.q;
-  var location = request.param('geo-city');	
+  var location = request.params.city;	
   console.log(location);
   getWeather(location).then(weatherJson => {
 		  var weatherDetails = '';
@@ -67,7 +67,7 @@ app.get('/webhook', function(request, response) {
 
 app.post('/webhook', function(request, response) {
   //var location = request.query.q;
-  var location = request.params.geo-city;	
+  var location = request.params.city;	
   console.log(location);
   getWeather(location).then(weatherJson => {
 		  var weatherDetails = '';

9fe507dc2d497c476b41c5dedf85e23d766b0e87
J Middya <jmiddya.mca@gmail.com>
2017-07-04 14:54:42 +0530

    Update test.js

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-app.engine('html', require('ejs').renderFile);
+//app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
@@ -92,9 +92,9 @@ app.post('/webhook', function(request, response) {
 		  "source": "Uber"
 		  }
 	//////////////// Preparing output JSON : END /////////////////	
-	response..render("/index.html");	
+	//response..render("/index.html");	
 	//response.sendFile(path.join(__dirname + '/index.html'));	  
-  	//response.send(outJSON); 
+  	response.send(outJSON); 
   }	  
 	
   getWeather(location).then(weatherJson => {

0bc7d4dcc813dec120dc533c2361e4f46bb80973
J Middya <jmiddya.mca@gmail.com>
2017-07-06 23:46:43 +0530

    Update test.js

--- test.js
+++ test.js
@@ -24,6 +24,301 @@ const VALIDATION_TOKEN = config.get('validationToken');
 // Generate a page access token for your page from the App Dashboard
 const PAGE_ACCESS_TOKEN = config.get('pageAccessToken');
 
+//////////////// Code for Uber Booking : START /////////////
+
+var getEstimatesForUserLocation = function(userLatitude, userLongitude, dropLatitude, dropLongitude, access_token, estimates) {
+	let parsedData = '';
+	console.log("Requesting updated time estimate...");
+	
+	console.log(access_token);
+	console.log(userLatitude);
+	console.log(userLongitude);
+	console.log(dropLatitude);
+	console.log(dropLongitude);
+	
+	var qs = require("querystring");
+	//var http = require("https");
+
+	///////////////////// For Products : START //////////////////////
+	var options = {
+	  "method": "GET",
+	  "hostname": "sandbox-api.uber.com",
+	  //"port": null,
+	  "path": '/v1.2/products?latitude='+userLatitude+'&longitude='+userLongitude,
+	  "headers": {
+		'Authorization': "Bearer " + access_token,
+		'Accept-Language': "en_US",
+		'content-type': "application/json",
+	  }
+	};
+
+	var req = https.request(options, function (res) {
+	  	
+	  var chunks = [];
+
+	  res.on("data", function (chunk) {
+		//console.log('chunk'+chunk);  
+		chunks.push(chunk);
+	  });
+
+	  res.on("end", function () {
+		var body = Buffer.concat(chunks);
+		var json = JSON.parse(body);
+		//var json = JSON.stringify(body);
+		console.log('Products'+chunks);
+		global.Products = json;
+		if(json) global.Product_id = json["products"][0]["product_id"];
+		parsedData = json;
+		estimates(json);
+		//console.log('Products'+Products);
+		//console.log(Product_id);	
+		
+	  });
+	});
+
+	req.end();
+	///////////////////// For Products : END //////////////////////	
+
+//return parsedData;  
+}
+
+app.get('/bookuber', function(req, res) {
+	var state = req.query.state;
+	var code = req.query.code;
+	if(state == 'mapView') {
+	//var myObject = { title:state + ' : ', supplies:['X','Y','Z'], estimates:code };
+	var myObject = { code:code };
+	res.render('test.ejs', myObject );
+	}
+});
+
+app.get('/getPrice', function(req, res) {
+	var userLatitude = req.query.start_latitude;
+	var userLongitude = req.query.start_longitude;
+	var dropLatitude = req.query.end_latitude;
+	var dropLongitude = req.query.end_longitude;
+	var product_id = req.query.product_id;
+
+	console.log("getPrice of "+product_id);
+	
+	var options = {
+	  "method": "POST",
+	  "hostname": "sandbox-api.uber.com",
+	  "path": '/v1.2/requests/estimate',
+	  "headers": {
+		'Authorization': "Bearer " + access_token,
+		'Accept-Language': "en_US",
+		'Content-Type': "application/json"
+	  }
+	};
+
+	var req3 = https.request(options, function (res2) {
+	  console.log("res"+res2);	
+	  var chunks = [];
+
+	  res2.on("data", function (chunk) {
+		//console.log('chunk'+JSON.stringify(chunk));  
+		chunks.push(chunk);
+	  });
+
+	  res2.on("end", function () {
+		var body = Buffer.concat(chunks);
+		var json = JSON.parse(body);
+		console.log('Fair_ID : '+json["fare"]["fare_id"]);	
+		res.send(json);
+		//console.log(body);
+		//console.log(body.toString());
+		//console.log(json);
+		//console.log(json.toString());
+		
+	  });
+	});
+
+	req3.write(JSON.stringify({
+	  product_id: Product_id,	
+	  start_latitude: userLatitude,
+	  start_longitude: userLongitude,
+	  end_latitude: dropLatitude,
+	  end_longitude: dropLongitude }));
+	  
+	req3.end();
+});
+
+app.get('/confirmRequest', function(req, res) {
+	var userLatitude = req.query.start_latitude;
+	var userLongitude = req.query.start_longitude;
+	var dropLatitude = req.query.end_latitude;
+	var dropLongitude = req.query.end_longitude;
+	var fare_id = req.query.fare_id;
+
+	console.log("Request of "+fare_id);
+	
+	var options = {
+	  "method": "POST",
+	  "hostname": "sandbox-api.uber.com",
+	  "path": '/v1.2/requests',
+	  "headers": {
+		'Authorization': "Bearer " + access_token,
+		'Accept-Language': "en_US",
+		'Content-Type': "application/json"
+	  }
+	};
+
+	var req3 = https.request(options, function (res2) {
+	  console.log("res"+res2);	
+	  var chunks = [];
+
+	  res2.on("data", function (chunk) {
+		//console.log('chunk'+JSON.stringify(chunk));  
+		chunks.push(chunk);
+	  });
+
+	  res2.on("end", function () {
+		var body = Buffer.concat(chunks);
+		var json = JSON.parse(body);
+		//console.log('Fair_ID : '+json["fare"]["fare_id"]);	
+		res.send(json);
+		//console.log(body);
+		//console.log(body.toString());
+		//console.log(json);
+		//console.log(json.toString());
+		
+	  });
+	});
+
+	req3.write(JSON.stringify({
+	  fare_id: fare_id,	
+	  start_latitude: userLatitude,
+	  start_longitude: userLongitude,
+	  end_latitude: dropLatitude,
+	  end_longitude: dropLongitude }));
+	  
+	req3.end();
+});
+
+app.get('/surge_confirmation', function(req, res) {
+	console.log(res);
+});
+
+app.get('/cancell', function(req, res) {
+	var options = {
+			  "method": "DELETE",
+			  "hostname": "sandbox-api.uber.com",
+			  "path": '/v1.2/requests/current',
+			  "headers": {
+				'Authorization': "Bearer " + access_token,
+				'Accept-Language': "en_US",
+				'Content-Type': "application/json"
+			  }
+			};
+
+			var req = https.request(options, function (res2) {
+			  console.log("res3"+res);	
+			  var chunks = [];
+
+			  res2.on("data", function (chunk) {
+				//console.log('chunk'+JSON.stringify(chunk));  
+				chunks.push(chunk);
+			  });
+
+			  res2.on("end", function () {
+			  console.log('Cancelled');
+			  res.send('Cancelled');
+			  });
+			});
+
+			req.end();
+});	
+
+app.get('/uber', function(req, res) {
+var productList = '';	
+var allEstimate = '';
+
+var userLatitude = req.query.start_latitude;
+var userLongitude = req.query.start_longitude;
+var dropLatitude = req.query.end_latitude;
+var dropLongitude = req.query.end_longitude;
+var code = req.query.code;
+
+console.log(code);
+
+//////////////// Added to get Access Token using Authorization code : START //////////////////
+
+var qs = require("querystring");
+//var http = require("https");
+
+var options = {
+  "method": "POST",
+  "hostname": "login.uber.com",
+  //"port": null,
+  "path": "/oauth/v2/token",
+  "headers": {
+    "content-type": "application/x-www-form-urlencoded"
+  }
+};
+
+var req2 = https.request(options, function (res2) {
+  var chunks = [];
+
+  res2.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res2.on("end", function () {
+    var body = Buffer.concat(chunks);
+	var json = JSON.parse(body);
+	console.log("access_token1"+access_token);
+	if(access_token == '') access_token = json.access_token;
+	console.log("access_token2"+access_token);
+
+	///////////// Added to get Estimatiion : START /////////////
+	productList = getEstimatesForUserLocation(userLatitude, userLongitude, dropLatitude, dropLongitude, access_token, function (estimates)
+	{
+		console.log('Called : '+estimates);
+		//var allEstimate = '';
+		var data = estimates["products"]; 
+			//console.log(data);
+			if (typeof data != typeof undefined) {
+				// Sort Uber products by time to the user's location 
+				/*data.sort(function(t0, t1) {
+					return t0.duration - t1.duration;
+				});*/
+
+				data.forEach(displayProducts);
+				//console.log("Updating time estimate...");
+				var checked = '';
+				function displayProducts(item, index) {
+					if(index == 0) {
+						checked = 'checked';
+					} else {
+						checked = '';
+					}	
+					allEstimate += '<input type="radio" name="product" value="' + item.product_id + '" ' + checked + '>' + item.display_name + " " + '<img src="' + item.image + '" alt="' + item.display_name + '" width="40" > '; 
+					//alert(allEstimate);
+				}
+							
+			}
+		
+		console.log(allEstimate);
+		res.send(allEstimate+'<input type="button" name="estimate" id="estimate" value="Get Estimate" onclick="getEstimatesForSelectedVehicle();" >');	
+	});
+	///////////// Added to get Estimatiion : END ///////////////  
+	
+  });
+});
+
+req2.write(qs.stringify({ code: code,
+  client_id: 'q5XFNcuI0FffOKwN79Im1WStCTJPeY-d',
+  client_secret: 'hBPm-ocnBDS23XSc6B1zLwjKLcJPYYnm5oKMH8_u',
+  redirect_uri: 'https://my-demo-bot.herokuapp.com/bookuber',
+  grant_type: 'authorization_code' }));
+
+req2.end();	
+	
+///////////////// Added to get Access Token using Authorization code : END ////////////////
+});
+//////////////// Code for Uber Booking : END ///////////////
+
 var getWeather = function ( location) {
   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=f953e7b081a49dc14a56671ffa303848';
   return fetch(url, {
